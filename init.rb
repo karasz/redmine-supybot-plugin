39f6f6e37b26e12e3a9c5c2feb14c754b0abd042
Nagy Karoly Gabriel <nagy.karoly@expert-erp.net>
2009-04-15 23:46:15 +0300

    begun implementing settings

--- init.rb
+++ init.rb
@@ -6,5 +6,11 @@ Redmine::Plugin.register :redmine_supy do
   author 'Nagy Karoly Gabriel'
   description 'Announce to Supybot via ExternalControl'
   version '0.0.1'
+  settings :default => {
+    'supy_binary' => '',
+    'supy_network' => 'freenode',
+    'supy_port' => '',
+    'supy_channel' => '#your channel'
+  }, :partial => 'settings/redminesupy_settings'
 end
 
